b711a21a5ee8b5537afec83d16b7ffcbea56747c
Mathieu Lutfy <mathieu@bidon.ca>
2014-08-19 14:57:48 -0400

    regionlookup.js: Start rewrite towards a clearner jquery-friendly way of exposing the crmRegionLookup() function. Also added handling of ajax-loaded elements, i.e. we check for ajaxComplete() event. Useful for the multiple address blocks in the Contact/Edit form.

--- regionlookup.js
+++ regionlookup.js
@@ -4,26 +4,49 @@ cj(function ($) {
     return;
   }
 
-  $(CRM.regionlookup.source).change(function() {
-    if (! $(this).val()) {
-      return;
-    }
+  cj.fn.crmRegionLookup = function() {
+    return this.each(function() {
+      $this = cj(this);
 
-    var query = CRM.url('civicrm/regionlookup/postcode/') + $(this).val() + '.json';
+      if ($this.hasClass('crm-regionlookup-processed')) {
+        return;
+      }
 
-    $.getJSON(query, function(data) {
-      if (data) {
-        $.each(data, function(key, val) {
-          if (key != 'source' && CRM.regionlookup[key]) {
-            $(CRM.regionlookup[key]).val(val).change();
+      $this.addClass('crm-regionlookup-processed');
+
+      $this.change(function() {
+        if (! $this.val()) {
+          return;
+        }
+
+        var query = CRM.url('civicrm/regionlookup/postcode/') + $this.val() + '.json';
+
+        $.getJSON(query, function(data) {
+          if (data) {
+            $.each(data, function(key, val) {
+              if (key != 'source' && CRM.regionlookup[key]) {
+                $(CRM.regionlookup[key]).val(val).change();
+              }
+            });
+          }
+
+          // Call a custom callback function, if any.
+          // FIXME: this sounds really wrong.
+          // Should be an anon function in the $(foo).crmRegionLookup() call?
+          // FIXME: do not rely on $this being passed as an argument. Need to find a cleaner way.
+          if (CRM.regionlookup.callback) {
+            eval(CRM.regionlookup.callback + '(data, $this)');
           }
         });
-      }
-
-      // Call a custom callback function, if any
-      if (CRM.regionlookup.callback) {
-        eval(CRM.regionlookup.callback + '(data)');
-      }
+      });
     });
+  };
+
+  // Always enable on the main element
+  cj(CRM.regionlookup.source).crmRegionLookup();
+
+  // After ajax calls, check to see if there are new elements to handle.
+  $(document).ajaxComplete(function(event, request, settings) {
+    cj(CRM.regionlookup.source).crmRegionLookup();
   });
 });
